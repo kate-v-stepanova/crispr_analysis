74d25bb39f5850b4550fc78d66003de4eec38d74
Ekaterina Stepanova <kate-v-stepanova@github.com>
2018-10-25 09:10:04 +0000

    Set up redis - additional script and change requirements

---
+++ utils/check_server.sh
@@ -0,0 +1,19 @@
+export TZ=Europe/Berlin
+now=$(date)
+if pgrep -x "redis-server" > /dev/null
+then
+	echo $now ': Redis Running'
+else
+	echo $now: ': Redis Restarting'
+	redis-server &
+fi
+if pgrep -x "flask" > /dev/null
+then
+    echo $now ': Flask Running'
+else
+	echo $now ": Restarting"
+	source activate crispr_analysis
+	export FLASK_APP=crispr_analysis.py
+	export FLASK_ENV=development
+	flask run --host 0.0.0.0 &
+fi
