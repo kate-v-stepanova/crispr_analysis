6770df335770af50886f03e990b6d8b391bfe4dd
kate-v-stepanova <kate-v-stepanova@github.com>
2019-01-21 11:25:03 +0100

    Data Import page: add a warning

--- static/js/data_import.js
+++ static/js/data_import.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
     $(document).on('click', 'span.close', function() {
         $(this).closest('div.alert').addClass('d-none');
     });
+
+    $(document).on('submit', '#import_form', function() {
+        alert('Please, note that data import might take between 30 minutes and 2 hours (in average). DO NOT CLOSE the browser, DO NOT RELOAD the page, DO NOT click any buttons. Just get some tea and wait till you see a green message upon success or red message upon a failed import');
+    });
 });
