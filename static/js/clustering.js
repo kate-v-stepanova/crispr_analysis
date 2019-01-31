0cda6f7c354d830df8b3ed76a482676b002dfe26
kate-v-stepanova <kate-v-stepanova@github.com>
2019-01-31 17:03:06 +0100

    Clustering: scatter plot (colors for clusters and shapes for cell lines)

--- static/js/clustering.js
+++ static/js/clustering.js
@@ -19,34 +19,39 @@ $(document).ready(function() {
         }
     });
 
+    // close error message
+    $(document).on('click', 'span.close', function() {
+        $(this).closest('div.alert').addClass('d-none');
+    });
 
     // initializing constants and removing attributes from html elements
     var PLOT_SERIES = $('#cluster_plot').attr('data-plot-series').replace(/'/g, '"'); //");
-    console.log(PLOT_SERIES);
     if (PLOT_SERIES.length != 0) {
         PLOT_SERIES = JSON.parse(PLOT_SERIES);
     }
     var GENES = $('#cluster_plot').attr('data-genes').replace(/'/g, '"'); //");
-    console.log(GENES);
     if (GENES.length != 0) {
         GENES = JSON.parse(GENES);
     }
-//    var CELL_LINES = $('#cluster_plot').attr('data-cell-lines').replace(/'/g, '"'); //");
+    var CELL_LINES = $('#cluster_plot').attr('data-cell-lines').replace(/'/g, '"'); //");
+    if (CELL_LINES.length != 0) {
+        CELL_LINES = JSON.parse(CELL_LINES);
+    }
     // removing attributes
     $('#cluster_plot').removeAttr('data-genes');
     $('#cluster_plot').removeAttr('data-plot-series');
-//    $('#cluster_plot').removeAttr('data-cell-lines');
+    $('#cluster_plot').removeAttr('data-cell-lines');
+
 
     if (GENES.length != 0) {
         // initializing plot
-        var cell_lines = $('#multiple_cell_lines').val();
         var chart = Highcharts.chart('cluster_plot', {
             chart: {
                 type: 'scatter',
-                zoomType: 'xy'
+                zoomType: 'xy',
             },
             title: {
-                text: 'Gene clusters for selected cell lines: <b>' + cell_lines + '</b>'
+                text: 'Clustered genes from cell lines: <b>' + CELL_LINES + '</b>'
             },
             xAxis: {
                 title: {
@@ -60,21 +65,21 @@ $(document).ready(function() {
             },
             yAxis: {
                 title: {
-                    text: 'Fold change of ' + cell_lines[0]
+                    text: 'Fold change'
                 }
             },
-    //        legend: {
-    //            labelFormatter: function () {
-    //                return this.name + ': ' + this.options.series_length +' genes <i>(click to hide)</i>';
-    //            }
-    //        },
+            legend: {
+                labelFormatter: function () {
+                    return this.name + ': ' + this.options.series_length +' genes <i>(click to hide)</i>';
+                }
+            },
             plotOptions: {
                 scatter: {
-    //                point: {
-    //                    events: {
-    //                        click: renderBoxplot
-    //                    }
-    //                },
+                    point: {
+                        events: {
+                            click: renderBoxplot
+                        }
+                    },
                     marker: {
                         radius: 5,
                         states: {
@@ -92,13 +97,159 @@ $(document).ready(function() {
                         }
                     },
                     tooltip: {
-                        headerFormat: '<b>gene: {point.x}</b><br><b>cell line: {series.name}<br></b>',
-                        pointFormat: '<b>fc value:</b> {point.y}<br><b>p value:</b> {point.pval}' +
-                                    '<br><b>increased Essentiality:</b> {point.inc_ess}<br>(click to see norm. counts)',
+                        headerFormat: '',
+                        pointFormat: '<b>Cluster {point.cluster} ({point.cluster_size} genes)<br></b><b>gene:</b> {point.gene_id}<br><b>cell line:</b> {point.cell_line}<br>' +
+                                    '<b>fc value:</b> {point.y}<br><b>p value:</b> {point.pval}' +
+                                    '<br>(click to see norm. counts)',
                     }
                 }
             },
-            series: PLOT_SERIES
+            series: PLOT_SERIES,
+            legend: {
+                enabled: false,
+            }
+        });
+
+        // initializing plot
+        var chart = Highcharts.chart('cluster_plot_small', {
+            chart: {
+                type: 'scatter',
+                zoomType: 'xy'
+            },
+            legend: {
+                align: 'right',
+                verticalAlign: 'top',
+                layout: 'vertical',
+                x: 0,
+                y: 100
+            },
+            title: {
+                text: 'Clustered genes from cell lines: <b>' + CELL_LINES + '</b>'
+            },
+            xAxis: {
+                title: {
+                    enabled: true,
+                    text: 'Genes'
+                },
+                categories: GENES,
+                startOnTick: true,
+                endOnTick: true,
+                showLastLabel: true
+            },
+            yAxis: {
+                title: {
+                    text: 'Fold change'
+                }
+            },
+            legend: {
+                labelFormatter: function () {
+                    return this.name + ': ' + this.options.series_length +' genes <i>(click to hide)</i>';
+                }
+            },
+            plotOptions: {
+                scatter: {
+                    point: {
+                        events: {
+                            click: renderBoxplot
+                        }
+                    },
+                    marker: {
+                        radius: 5,
+                        states: {
+                            hover: {
+                                enabled: true,
+                                lineColor: 'rgb(100,100,100)'
+                            }
+                        }
+                    },
+                    states: {
+                        hover: {
+                            marker: {
+                                enabled: false
+                            }
+                        }
+                    },
+                    tooltip: {
+                        headerFormat: '',
+                        pointFormat: '<b>Cluster {point.cluster} ({point.cluster_size} genes)<br></b><b>gene:</b> {point.gene_id}<br><b>cell line:</b> {point.cell_line}<br>' +
+                                    '<b>fc value:</b> {point.y}<br><b>p value:</b> {point.pval}' +
+                                    '<br>(click to see norm. counts)',
+                    }
+                }
+            },
+            series: PLOT_SERIES,
+        });
+    }
+
+    // resize cell_line_chart when hide counts
+    $('#hide_counts').on('click', function() {
+        $('#hide_counts').addClass('d-none');
+        $('#plot_with_counts').addClass('d-none');
+        $('#cluster_plot').removeClass('d-none');
+    });
+
+    function renderBoxplot(e) {
+        var gene = this.gene_id;
+        var cell_lines = $('#multiple_cell_lines').val();
+        $('#plot_with_counts').removeClass('d-none');
+        $('#cluster_plot').addClass('d-none');
+        $('#hide_counts').removeClass('d-none');
+        $.post('/get_norm_counts/' + gene + "/" + cell_lines, function(data, status) {
+            if (status == 'success' && data.length != 0) {
+                if (data['errors'].length != 0) {
+                    $('#error_messages').html("");
+                    for (i=0; i<data['errors'].length; i++) {
+                        $('#error_messages').append("<p>"+data['errors'][i] + "</p>")
+                    }
+
+                    $('#error_div').removeClass('d-none');
+                }
+                Highcharts.chart('boxplot_data', {
+                    chart: {
+                        type: 'boxplot'
+                    },
+                    title: {
+                        text: 'Normalized Counts for gene: ' + gene
+                    },
+
+                    xAxis: {
+                        categories: CELL_LINES,
+                        title: {
+                            text: 'Cell Line'
+                        }
+                    },
+
+                    yAxis: {
+                        title: {
+                            text: 'Normalized counts'
+                        },
+                    },
+
+                    legend: {
+                        labelFormatter: function() {
+                            if (this.name == 'Outliers') {
+                                return 'click to hide outliers';
+                            } else {
+                                return this.name;
+                            }
+                        }
+                    },
+                    series: data['data'],
+                });
+            } else {
+                $('#boxplot_data').html('No data found for the gene <b>' + gene + "</b>");
+            }
+        });
+    }
+
+
+    var data_table = $('#data_table');
+    if (data_table.length != 0) {
+        var table_data = $(data_table).attr('table-csv-data');
+        $(data_table).removeAttr('table-csv-data');
+        $('#export_button').on('click', function() {
+            var blob = new Blob([table_data], {type: "text/plain;charset=utf-8"});
+            saveAs(blob, CELL_LINES.join('_') + "_clusters.csv");
         });
     }
 });
--- url_handlers/clustering.py
+++ url_handlers/clustering.py
@@ -19,6 +19,10 @@ def cluster_data():
         clustering_method = request.form.get('clustering_method')
         selected_filter = request.form.get('filter_data')
         selected_genes = request.form.get('selected_genes').strip().split()
+        number_of_clusters = request.form.get('number_of_clusters')
+        if number_of_clusters:
+            # i don't make any checks, because the form on submit already makes sure that entered number is int
+            number_of_clusters = int(number_of_clusters)
 
         # thresholds
         wt_fc_max = float(request.form.get('wt_fc_max'))
@@ -28,66 +32,114 @@ def cluster_data():
         fc_min = float(request.form.get('fc_min'))
         pval = float(request.form.get('pval_max'))
 
+        full_df = None
         wt_df = None
-        joint_df = None
-        for cell_line in selected_cell_lines:
-            data = rdb.get(cell_line)
-            if not data:
-                continue
-            df = pd.read_msgpack(data)
-            df = df[['gene_id', 'fc', 'pval']]
-            df.fc.astype(float)
-            df.pval.astype(float)
-            # Filter by genes
-            if selected_filter == 'by_genes':
-                df = df.loc[df['gene_id'].isin(selected_genes)].reset_index()
-            # Filter by thresholds
-            elif selected_filter == 'by_thresholds':
+        if selected_filter == 'by_thresholds':
+            selected_genes = []
+            for cell_line in selected_cell_lines:
+                data = rdb.get(cell_line)
+                if not data:
+                    continue
+                df = pd.read_msgpack(data)
+
+                df = df[['gene_id', 'fc', 'pval']]
+                df.columns = ['gene_id', '{}_fc'.format(cell_line), '{}_pval'.format(cell_line)]
+                full_df = df.copy() if full_df is None else pd.merge(full_df, df, how='outer', on='gene_id')
+
+                # Filter by thresholds
                 if 'WT' in cell_line:
-                    df = df.loc[df['fc'] >= wt_fc_min]
-                    df = df.loc[df['fc'] <= wt_fc_max]
-                    df = df.loc[df['pval'] <= wt_pval]
+                    df = df.loc[df['{}_fc'.format(cell_line)] >= wt_fc_min]
+                    df = df.loc[df['{}_fc'.format(cell_line)] <= wt_fc_max]
+                    df = df.loc[df['{}_pval'.format(cell_line)] <= wt_pval]
                 else:
-                    df = df.loc[df['fc'] >= fc_min]
-                    df = df.loc[df['fc'] <= fc_max]
-                    df = df.loc[df['pval'] <= pval]
-            df.columns = ['gene_id', '{}_fc'.format(cell_line), '{}_pval'.format(cell_line)]
-            if cell_line == 'WT':
-                wt_df = df
-            else:
-                joint_df = df.copy() if joint_df is None else pd.merge(joint_df, df, how='outer', on='gene_id')
+                    df = df.loc[df['{}_fc'.format(cell_line)] >= fc_min]
+                    df = df.loc[df['{}_fc'.format(cell_line)] <= fc_max]
+                    df = df.loc[df['{}_pval'.format(cell_line)] <= pval]
+
+                if 'WT' in cell_line:
+                    wt_df = df.copy()
+
+                selected_genes += list(df['gene_id'].unique())
+            #     selected_genes += list(df['gene_id'].unique())
+            selected_genes = list(set(selected_genes))
+            # df = full_df.loc[full_df['gene_id'].isin(selected_genes)].reset_index()
+        else:
+            for cell_line in selected_cell_lines:
+                data = rdb.get(cell_line)
+                if not data: continue
+                df = pd.read_msgpack(data)
+                df = df[['gene_id', 'fc', 'pval']]
+                df.columns = ['gene_id', '{}_fc'.format(cell_line), '{}_pval'.format(cell_line)]
+                if 'WT' in cell_line:
+                    wt_df = df.copy()
+                else:
+                    full_df = df.copy() if full_df is None else pd.merge(full_df, df, how='outer', on='gene_id')
 
         if wt_df is not None:
-            joint_df = pd.merge(joint_df, wt_df, how='inner', on='gene_id')
+            full_df = pd.merge(full_df, wt_df, how='inner', on='gene_id')
 
-        columns = list(joint_df.columns)
-        df_to_cluster = joint_df.drop(columns=['gene_id'])
-        df_to_cluster = df_to_cluster.dropna()
-        # Using sklearn
-        km = KMeans(n_clusters=3)
-        km.fit(df_to_cluster.values)
-        # Get cluster assignment labels
-        labels = km.labels_
-        # Format results as a DataFrame
-        results = pd.DataFrame([df_to_cluster.index, labels]).T
+        if selected_filter == 'do_not_filter':
+            df = full_df.copy()
+        elif selected_filter == 'by_thresholds':
+            df = full_df.loc[full_df['gene_id'].isin(selected_genes)].reset_index()
+            df = pd.merge(df, wt_df, how='inner', on='gene_id')
+        else:
+            df = full_df.loc[full_df['gene_id'].isin(selected_genes)].reset_index()
 
-        results = pd.concat([results, joint_df], axis=1)
-        # 1 is the name of the cluster column in results df
-        results = results[columns + [1]]
-        results.columns = columns + ['cluster']
+        # clustering
+        df = df.drop_duplicates()
 
-        results = results.fillna('null')
-        results['y'] = results['{}_fc'.format(selected_cell_lines[0])].copy()
+        df_to_cluster = df.drop(columns=['gene_id']).round(decimals=3)
+        kmeans = KMeans(n_clusters=number_of_clusters, random_state=0).fit(df_to_cluster)
+        labels = kmeans.labels_
+        df_to_cluster['cluster'] = labels
+        df_to_cluster['gene_id'] = df['gene_id']
+
         plot_series = []
-        for i in range(5):
-            df = results.loc[results['cluster'] == i]
+        shapes = ["circle", "square", "diamond", "triangle", "triangle-down"]
+        colors = ['#b3e6ff', '#ffe6b3', '#ccffb3', '#ffb3b3', '#d699ff', 'black', 'grey']
+        df_to_cluster = df_to_cluster.sort_values(by=['cluster'])
 
+        ordered_genes = []
+        for cell_line in selected_cell_lines:
+            df1 = df_to_cluster[['gene_id', '{}_fc'.format(cell_line), '{}_pval'.format(cell_line), 'cluster']].copy()
+            plot_data = []
+            for i in range(number_of_clusters):
+                df = df1.loc[df_to_cluster['cluster'] == i]
+                df.columns = ['gene_id', 'fc', 'pval', 'cluster']
+                df['y'] = df['fc']
+                df['name'] = df['gene_id']
+                df['cell_line'] = cell_line
+                df['color'] = colors[i % len(colors)]
+                df['cluster_size'] = len(df)
+                plot_data += list(df.T.to_dict().values())
+                ordered_genes += df['gene_id'].tolist()
             plot_series.append({
-                'name': 'cluster_{}'.format(i),
-                'data': list(df.T.to_dict().values()),
-                'turboThreshold': len(df),
+                # 'name': 'Cluster {}'.format(i),
+                'turboThreshold': len(df_to_cluster),
+                'data': plot_data,
+                'marker': {
+                    'symbol': shapes[selected_cell_lines.index(cell_line) % len(selected_cell_lines)]
+                }
             })
-        selected_genes = results['gene_id'].tolist()
+
+        df_to_cluster.insert(0, '#', range(1, len(df_to_cluster)+1))
+        columns = []
+        for cell_line in selected_cell_lines:
+            columns += ['{}_fc'.format(cell_line), '{}_pval'.format(cell_line)]
+        df_to_cluster = df_to_cluster[['#', 'gene_id', 'cluster'] + columns]
+        data_table = {
+            'header': df_to_cluster.columns,
+            'rows': df_to_cluster.values.tolist(),
+            'csv': df_to_cluster.to_csv(sep='\t', index=False)
+        }
+
+        selected_genes = ordered_genes
+        filtered_genes = None
+        if selected_filter == 'by_thresholds':
+            filtered_genes = selected_genes
+            selected_genes = None
+
         return render_template('clustering.html',
                                cell_lines=cell_lines,
                                selected_cell_lines=selected_cell_lines,
@@ -102,6 +154,94 @@ def cluster_data():
                                    'pval': pval
                                },
                                selected_genes=selected_genes,
-                               plot_series=plot_series)
+                               filtered_genes=filtered_genes,
+                               plot_series=plot_series,
+                               number_of_clusters=number_of_clusters,
+                               data_table=data_table)
 
     return render_template('clustering.html', cell_lines=cell_lines, selected_filter="do_not_filter")
+
+
+# js post request - called on selected an entity from a list
+@cluster_page.route('/get_norm_counts/<gene>/<cell_lines>', methods=['POST'])
+def get_norm_counts(gene, cell_lines):
+    # this import has to be here!!
+    from crispr_analysis import get_db
+    rdb = get_db()
+    cell_lines = cell_lines.split(',')
+    msgpack = rdb.get('{}_counts'.format(gene))
+    if msgpack is None:
+        return jsonify({
+            'data': [],
+            'errors': ['No counts for gene {} found'.format(gene)]
+        })
+    gene_df = pd.read_msgpack(msgpack)
+    series_before = []
+    series_after = []
+    outliers = []
+    error_messages = []
+    for i in range(len(cell_lines)):
+        cell_line = cell_lines[i]
+        # key = 'RPE_{}'.format(cell_line)
+        df = gene_df.loc[gene_df['cell_line'] == cell_line]
+        # if df.empty:
+        #     key = cell_line
+        #     df = gene_df.loc[gene_df['cell_line'] == key]
+        if df.empty:
+            error_messages.append('No counts for gene: {} and cell line: {}. '.format(gene, cell_line))
+            continue
+
+        # keep only the ones that are within +3 to -3 standard deviations
+        without_outliers = df[np.abs(df.norm_counts - df.norm_counts.mean()) <= (3 * df.norm_counts.std())]
+        only_outliers = df[np.abs(df.norm_counts - df.norm_counts.mean()) > (3 * df.norm_counts.std())]
+        before = without_outliers.loc[without_outliers['treatment'] == 0]
+        after = without_outliers.loc[without_outliers['treatment'] == 1]
+
+        # calculate boxplot data
+        q1, median, q3 = before.norm_counts.quantile([0.25, 0.5, 0.75]).round(decimals=3).tolist()
+        series_before.append([
+            before['norm_counts'].min().round(decimals=3),
+            q1,
+            median,
+            q3,
+            before['norm_counts'].max().round(decimals=3)])
+        q1, median, q3 = after.norm_counts.quantile([0.25, 0.5, 0.75]).round(decimals=3).tolist()
+        series_after.append([
+                after['norm_counts'].min().round(decimals=3),
+                q1,
+                median,
+                q3,
+                after['norm_counts'].max().round(decimals=3)])
+        for index, row in only_outliers.iterrows():
+            x = i
+            y = round(row['norm_counts'], 3)
+            treatment = 'Before Treatment' if int(row['treatment']) == 0 else 'After Treatment'
+            outliers.append({
+                'x': x,
+                'y': y,
+                'treatment': treatment,
+                'cell_line': cell_line,
+                'color': 'black' if treatment == 'After Treatment' else '#7cb5ec'
+            })
+
+    counts_series = [{
+            'name': 'Before Treatment',
+            'data': series_before,
+            'color': '#7cb5ec',
+        }, {
+            'name': 'After Treatment',
+            'data': series_after,
+            'color': 'black',
+        }, {'name': 'Outliers',
+            'type': 'scatter',
+            'data': outliers,
+            'color': 'black',
+            'tooltip': {
+                'pointFormat': '<br>cell line: {point.cell_line}<br>norm. counts: {point.y}<br>treatment: {point.treatment}',
+            }
+        }
+    ]
+    return jsonify({
+        'data': counts_series,
+        'errors': error_messages,
+    })
